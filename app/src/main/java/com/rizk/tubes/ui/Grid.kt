f0f1c394979e142df316e275d52df3efeda07285
Yohan Rizk <yrizk@yrizk123.roam.corp.google.com>
2018-10-18 09:13:48 -0400

    increased stroke width to fix the drawLine call not working

--- app/src/main/java/com/rizk/tubes/ui/Grid.kt
+++ app/src/main/java/com/rizk/tubes/ui/Grid.kt
@@ -22,10 +22,17 @@ class Grid(context: Context?) : ViewGroup(context) {
     // at 2/3rd marker of the width
     private var rightVerticalLine: Float = 0f
 
+    // at 1/3rd of the height
+    private var topHorizontalLine: Float = 0f
+
+    // at 2/3rd of the height
+    private var bottomHorizontalLine: Float = 0f;
+
     private val paint: Paint = Paint()
 
     init {
         paint.color = Color.RED
+        paint.strokeWidth = 10f
     }
 
     // do not allow scrolling
@@ -33,25 +40,23 @@ class Grid(context: Context?) : ViewGroup(context) {
         return false;
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        Log.v(TAG, "onMeasure")
-    }
-
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         // normally, we'd obey padding set, but I don't plan on even using that, so we can skip that calculation.
-        leftVerticalLine = width / 3f;
-        rightVerticalLine = leftVerticalLine * 2;
-        Log.v(TAG, "leftVerticalLine $leftVerticalLine, rightVerticalLine $rightVerticalLine")
+        leftVerticalLine = width / 3f
+        rightVerticalLine = leftVerticalLine * 2
+        topHorizontalLine = height / 3f
+        bottomHorizontalLine = topHorizontalLine * 2
+        Log.v(TAG, "leftVerticalLine $leftVerticalLine, rightVerticalLine $rightVerticalLine topHorizontalLine $topHorizontalLine, bottomHorizontalLine $bottomHorizontalLine")
         //todo: don't forget about laying the children
     }
 
     override fun dispatchDraw(canvas: Canvas) {
         super.dispatchDraw(canvas)
-        Log.v(TAG, "onDraw")
         //todo use the batch drawLine call
         canvas.drawLine(leftVerticalLine, 0f, leftVerticalLine, height.toFloat(), paint)
         canvas.drawLine(rightVerticalLine, 0f, rightVerticalLine, height.toFloat(), paint)
+        canvas.drawLine(0f, topHorizontalLine, width.toFloat(), topHorizontalLine, paint)
+        canvas.drawLine(0f, bottomHorizontalLine, width.toFloat(), bottomHorizontalLine, paint)
     }
 
 }
