63da194848c33ff00ea209b1946d8c4ea4d0b848
Yohan Rizk <yrizk@yrizk123.roam.corp.google.com>
2018-10-26 13:30:48 -0400

    removed the weird layout params call in addview

--- app/src/main/java/com/rizk/tubes/MainActivity.kt
+++ app/src/main/java/com/rizk/tubes/MainActivity.kt
@@ -2,7 +2,6 @@ package com.rizk.tubes
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.ViewGroup
 import com.rizk.tubes.ui.Grid
 import com.rizk.tubes.ui.Tube
 
@@ -13,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var grid = findViewById<Grid>(R.id.grid)
-        grid.addView(Tube(this), ViewGroup.MarginLayoutParams(grid.measuredWidth / 3, grid.measuredHeight / 3))
+        grid.addView(Tube(this))
     }
+
 }
